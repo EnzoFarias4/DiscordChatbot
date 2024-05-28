963e812de7fd0dbc8cc9b427197f4de1d5d2b570
EnzoFarias4 <102108717+EnzoFarias4@users.noreply.github.com>
2024-05-28 19:10:17 +0300

    update variable names

--- index.js
+++ index.js
@@ -2,24 +2,26 @@ require('dotenv').config();
 const fs = require('fs');
 const { Client, Collection, Intents } = require('discord.js');
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const discordBotClient = new Client({
+    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
+});
 
-client.commands = new Collection();
+discordBotClient.commands = new Collection();
 
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-for (const file of commandFiles) {
-    const command = require(`./commands/${file}`);
-    client.commands.set(command.data.name, command);
+for (const fileName of commandFiles) {
+    const commandModule = require(`./commands/${fileName}`);
+    discordBotClient.commands.set(commandModule.data.name, commandModule);
 }
 
 const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
-for (const file of eventFiles) {
-    const event = require(`./events/${file}`);
-    if (event.once) {
-        client.once(event.name, (...args) => event.execute(...args));
+for (const eventName of eventFiles) {
+    const eventModule = require(`./events/${eventName}`);
+    if (eventModule.once) {
+        discordBotClient.once(eventModule.name, (...args) => eventModule.execute(...urlargs));
     } else {
-        client.on(event.name, (...args) => event.execute(...args));
+        discordBotClient.on(eventModule.name, (...args) => eventModule.execute(...args));
     }
 }
 
-client.login(process.env.DISCORD_TOKEN);
+discordBotClient.login(process.env.DISCORD_TOKEN);
