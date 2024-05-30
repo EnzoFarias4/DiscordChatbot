1140125bbfeda4b89a1c32edfd3c90a9436eaaef
EnzoFarias4 <102108717+EnzoFarias4@users.noreply.github.com>
2024-05-30 05:24:01 +0300

    update error catching

--- index.js
+++ index.js
@@ -8,20 +8,34 @@ const discordBotClient = new Client({
 
 discordBotClient.commands = new Collection();
 
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-for (const fileName of commandFiles) {
-    const commandModule = require(`./commands/${fileName}`);
-    discordBotClient.commands.set(commandModule.data.name, commandModule);
-}
-
-const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
-for (const eventName of eventFiles) {
-    const eventModule = require(`./events/${eventName}`);
-    if (eventModule.once) {
-        discordBotClient.once(eventModule.name, (...args) => eventModule.execute(...urlargs));
-    } else {
-        discordBotClient.on(eventModule.name, (...args) => eventModule.execute(...args));
+try {
+    const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+    for (const fileName of commandFiles) {
+        const commandModule = require(`./commands/${fileName}`);
+        discordBotClient.commands.set(commandModule.data.name, commandModule);
     }
+} catch (error) {
+    console.error("Error loading commands: ", error);
 }
 
-discordBotClient.login(process.env.DISCORD_TOKEN);
+try {
+    const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+    for (const eventName of eventFiles) {
+        const eventModule = require(`./events/${eventName}`);
+        
+        if (eventModule.once) {
+            discordBotClient.once(eventModule.name, (...args) => eventModule.execute(...args).catch(error => console.error(error)));
+        } else {
+            discordBotClient.on(eventModule.name, (...args) => eventModule.execute(...args).catch(error => console.error(error)));
+        }
+    }
+} catch (error) {
+    console.error("Error loading events: ", error);
+}
+
+discordBotClient.login(process.env.DISCORD_TOKEN).catch(error => {
+    console.error("Error logging in: ", error);
+});
+
+discordBotClient.on('error', error => console.error('Discord client error: ', error));
+discordBotClient.on('unhandledRejection', error => console.error('Unhandled promise rejection: ', error));
