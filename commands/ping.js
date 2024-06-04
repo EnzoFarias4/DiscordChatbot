88a9c3105e19c88e8ef00a34a21d4d3426e05cb2
EnzoFarias4 <102108717+EnzoFarias4@users.noreply.github.com>
2024-06-04 18:33:58 +0300

    optimization

--- commands/ping.js
+++ commands/ping.js
@@ -1,5 +1,8 @@
 const fs = require('fs');
 require('dotenv').config();
+
+const axios = require('axios');
+
 const executeCommand = (language) => {
     switch (language) {
         case 'python':
@@ -12,6 +15,17 @@ const executeCommand = (language) => {
             console.log('Language not supported');
     }
 };
+
+const makeEfficientAPICall = async (requests) => {
+    try {
+        const responses = await Promise.all(requests.map(req => axios(req)));
+        console.log("API calls executed efficiently", responses);
+    } catch (error) {
+        console.error("Error making API calls", error);
+    }
+};
+
 module.exports = {
-    executeCommand
+    executeCommand,
+    makeEfficientAPICall
 };
