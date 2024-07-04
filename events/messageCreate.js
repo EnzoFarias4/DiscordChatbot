173cfcad40b8914ba537aa79886a813484963293
EnzoFarias4 <102108717+EnzoFarias4@users.noreply.github.com>
2024-07-04 07:50:07 +0300

    optimization improvement

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,33 +1,41 @@
 const EventEmitter = require('events');
-const axios = require('axios'); // Assuming axios for API calls
+const axios = require('axios'); 
 require('dotenv').config();
 
 class MyEventEmitter extends EventEmitter {}
 
 const myEventEmitter = new MyEventEmitter();
 
+const privateData = new WeakMap();
+
+privateData.set(myEventEmitter, {
+  apiCallQueue: []
+});
+
 myEventEmitter.on('batchProcess', async (apiRequests) => {
   console.log('Processing batched API requests...');
   try {
-    const responses = await Promise.all(apiRequests.map(req => axios(req)));
+    const responses = await Promise.all([...apiRequests].map(req => axios(req)));
     console.log('Batch request successful!', responses);
   } catch (error) {
     console.error('Batch request failed:', error);
   }
 });
 
-myEventEmitter.emit('event');
-
 console.log(`Your Environment Variable Directive: ${process.env.MY_VARIABLE}`);
 
-let apiCallQueue = [];
 function queueApiCall(apiRequest) {
-  apiCallQueue.push(apiRequest);
+  const data = privateData.get(myEventEmitter);
+  if (data) {
+    data.apiCallQueue.push(apiRequest);
+  }
 }
 
 setInterval(() => {
-  if (apiCallQueue.length > 0) {
-    myEventEmitter.emit('batchProcess', apiCallQueue);
-    apiCallQueue = [];
+  const data = privateData.get(myEventEmitter);
+  if (data && data.apiCallQueue.length > 0) {
+    const processQueue = [...data.apiCallQueue];
+    data.apiCallQueue.length = 0; 
+    myEventEmitter.emit('batchProcess', processQueue);
   }
 }, 10000);
